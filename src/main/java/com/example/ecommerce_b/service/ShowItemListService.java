c5cf62795d5440747b779a431a14bd5c78ba38ca
YamaokaHayato <hayatoyamaoka0804@gmail.com>
2022-11-28 12:14:11 +0900

    商品一覧情報を操作するサービスクラスの作成しitem情報を全件検索する業務処理と商品名を曖昧検索する業務処理を追加.

---
+++ src/main/java/com/example/ecommerce_b/service/ShowItemListService.java
@@ -0,0 +1,42 @@
+package com.example.ecommerce_b.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.ecommerce_b.domain.Item;
+import com.example.ecommerce_b.repository.ItemRepository;
+
+/**
+ * 商品一覧情報を操作するサービス.
+ * 
+ * @author yamaokahayato
+ *
+ */
+@Service
+@Transactional
+public class ShowItemListService {
+	
+	@Autowired
+	private ItemRepository itemRepository;
+	
+	/**
+	 * item情報を全件検索する業務処理と商品名を曖昧検索する業務処理.
+	 * 
+	 * @param name 商品名
+	 * @return 
+	 */
+	public List<Item> serchByName(String name) {
+		if (name == null) {
+			 List<Item> itemName = itemRepository.findByName(name);
+			 return itemName;
+		}
+		List<Item> itemList = itemRepository.findAll(); 
+		return itemList;
+	}
+	
+	
+
+}
