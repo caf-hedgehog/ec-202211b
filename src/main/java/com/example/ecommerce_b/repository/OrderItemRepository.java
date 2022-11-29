925c71325aee48d40b316d22582df4cdbc87205c
caf-hedgehog <hcejynny@gmail.com>
2022-11-29 13:44:56 +0900

    一時保存

--- src/main/java/com/example/ecommerce_b/repository/OrderItemRepository.java
+++ src/main/java/com/example/ecommerce_b/repository/OrderItemRepository.java
@@ -11,32 +11,29 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
-
 import com.example.ecommerce_b.domain.OrderItem;
 
 /**
  * Order_itemsテーブルを操作するレポジトリ.
  * 
  * @author 萩田
-
- * オーダー関連処理のレポジトリ.
+ * 
+ *         オーダー関連処理のレポジトリ.
  * 
  * @author shibatamasayuki
-
  *
+ * 
  */
 @Repository
 public class OrderItemRepository {
 
-
 	private static final RowMapper<OrderItem> ORDER_ITEM_ROW_MAPPER = new BeanPropertyRowMapper<>(OrderItem.class);
-
 
 	@Autowired
 	private NamedParameterJdbcTemplate template;
 
 	/**
-
+	 * 
 	 * 注文商品DB登録
 	 * 
 	 * @param orderItem 注文商品
@@ -44,7 +41,9 @@ public class OrderItemRepository {
 	public void insert(OrderItem orderItem) {
 		SqlParameterSource param = new BeanPropertySqlParameterSource(orderItem);
 		String sql = "INSERT INTO order_items(item_id, order_id, quantity, size) VALUES(:itemId, :orderId, :quantity, :size)";
+	}
 
+	/*
 	 * カート内商品を１件削除する.
 	 * 
 	 * @param id orderItemのid
