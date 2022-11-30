072cea9935d6c040d30b5f03de4cca0a489a5722
YamaokaHayato <hayatoyamaoka0804@gmail.com>
2022-11-30 16:25:38 +0900

    showListメソッドをPostMappingからRequestMappingに変更

--- src/main/java/com/example/ecommerce_b/controller/ShowItemListController.java
+++ src/main/java/com/example/ecommerce_b/controller/ShowItemListController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -24,7 +24,7 @@ public class ShowItemListController {
 
 	@Autowired
 	private ShowItemListService showItemListService;
-
+	
 	/**
 	 * 検索欄からの曖昧検索.
 	 * 
@@ -32,9 +32,9 @@ public class ShowItemListController {
 	 * @param model
 	 * @return 商品一覧画面
 	 */
-	@PostMapping("/showList")
-	public String showList(String itemName, Model model) {
-		List<Item> itemList = showItemListService.searchByName(itemName);
+	@RequestMapping("/showList")
+	public String showList(String name, Model model) {
+		List<Item> itemList = showItemListService.searchByName(name);
 		model.addAttribute("itemList", itemList);
 		return "item_list_curry";
 	}
