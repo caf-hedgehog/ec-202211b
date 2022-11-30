baec9a1e4d697154a2fb5e78142420c47d9b799a
YamaokaHayato <hayatoyamaoka0804@gmail.com>
2022-11-30 11:09:52 +0900

    ShowItemListコントローラーに並び替えた商品一覧画面を表示する内容を追加

--- src/main/java/com/example/ecommerce_b/controller/ShowItemListController.java
+++ src/main/java/com/example/ecommerce_b/controller/ShowItemListController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.ecommerce_b.domain.Item;
@@ -39,5 +40,21 @@ public class ShowItemListController {
 		model.addAttribute("itemList", itemList);
 		return "item_list_curry";
 	}
-
+	
+	/**
+	 * 商品の並び替え欄から送られた内容で商品の並び替えを表示する
+	 * 
+	 * @param order
+	 * @param model　モデル
+	 * @return 商品一覧画面
+	 */
+	@PostMapping("/itemSort") 
+	public String itemSort(String sort, Model model) {
+		System.out.println("sort : " + sort);
+		List<Item> itemList = showItemListService.itemSort(sort);
+		model.addAttribute("itemList", itemList);
+		System.out.println(itemList);
+		return "item_list_curry";
+	}
+	
 }
