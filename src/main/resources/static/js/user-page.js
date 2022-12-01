c037b75b1dff31b3cd9850f2cfbf89232d8c8a88
caf-hedgehog <hcejynny@gmail.com>
2022-12-01 18:03:07 +0900

    独自機能追加

---
+++ src/main/resources/static/js/user-page.js
@@ -0,0 +1,38 @@
+/**
+ * 住所検索
+ */
+
+"use strict";
+$(function () {
+  $("#btn_name").on("click", function () {
+    const input = document.getElementById("userName");
+    input.disabled = false;
+  });
+
+  $("#btn_email").on("click", function () {
+    const input = document.getElementById("email");
+    input.disabled = false;
+  });
+
+  $("#btn_zipcode1").on("click", function () {
+    const input = document.getElementById("inputZipcode");
+    input.disabled = false;
+    const btn = document.getElementById("btn_zipCode");
+    btn.disabled = false;
+  });
+
+  $("#btn_address").on("click", function () {
+    const input = document.getElementById("inputAddress");
+    input.disabled = false;
+  });
+
+  $("#btn_telephone").on("click", function () {
+    const input = document.getElementById("telephone");
+    input.disabled = false;
+  });
+
+  $("#btn_password").on("click", function () {
+    const input = document.getElementById("password");
+    input.disabled = false;
+  });
+});
